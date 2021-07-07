640dcbb63e2e727e5e5824783f999e3c43afe91d
lemoncube1 <lemonglasgroup@protonmail.com>
2021-07-07 19:40:23 +0300

    Init commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="public/app.css">
+    <title>My Blog</title>
+</head>
+<body>
+
+<article>
+    <h1>My First Post</h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem blanditiis eligendi error numquam possimus quos?
+        Architecto minima officiis quam, qui rem repudiandae voluptatibus. Aliquam enim qui quo unde vel, veritatis!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid asperiores doloribus fuga, id illo illum
+        impedit itaque mollitia natus nemo nesciunt nisi nostrum perferendis possimus, provident repellat tempora,
+        vel!</p>
+
+</article>
+
+</body>
+</html>
