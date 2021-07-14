5ea6c9c8241e21c9d7306c4954d7bbc5e73347bc
lemoncube1 <lemonglasgroup@protonmail.com>
2021-07-14 22:57:01 +0300

    complete episode 27

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -17,6 +17,12 @@ class Post extends Model
 
     }
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+
+    }
+
     public function getRouteKeyName()
     {
         return 'slug';
