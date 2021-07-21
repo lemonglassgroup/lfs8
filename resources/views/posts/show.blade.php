0e27ada7a440c2b5753e60eeebbb73f4574cf95d
lemoncube1 <lemonglasgroup@protonmail.com>
2021-07-21 12:07:37 +0300

    episode 42 complete

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,13 +7,16 @@
                     <img src="/images/illustration-1.png" alt="" class="rounded-xl">
 
                     <p class="mt-4 block text-gray-400 text-xs">
-                        Published <time>{{ $post->created_at->diffForHumans() }}</time>
+                        Published
+                        <time>{{ $post->created_at->diffForHumans() }}</time>
                     </p>
 
                     <div class="flex items-center lg:justify-center text-sm mt-4">
                         <img src="/images/lary-avatar.svg" alt="Lary avatar">
                         <div class="ml-3 text-left">
-                            <h5 class="font-bold">{{ $post->author->name }}</h5>
+                            <h5 class="font-bold">
+                                <a href="/?author={{ $post->author->username }}">{{ $post->author->name }}</a>
+                            </h5>
                         </div>
                     </div>
                 </div>
@@ -36,7 +39,7 @@
                         </a>
 
                         <div class="space-x-2">
-                            <x-category-button :category="$post->category" />
+                            <x-category-button :category="$post->category"/>
                         </div>
                     </div>
 
