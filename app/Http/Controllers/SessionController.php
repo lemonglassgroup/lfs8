a124e79409fad5b654abe343d247cdb09af946e5
lemoncube1 <lemonglasgroup@protonmail.com>
2021-07-22 11:49:57 +0300

    episode 50 complete

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,8 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
+use function Symfony\Component\String\s;
+
 class SessionController extends Controller
 {
+    public function create()
+    {
+        return view('session.create');
+    }
+
+    public function store()
+    {
+//        validate the request
+        $attributes = request()->validate([
+            'email' => 'required', // "exists:[table], [column]" provide privacy issue
+            'password' => 'required'
+        ]);
+//        authenticate and log in the user based on the provided credentials
+        if (auth()->attempt($attributes)) {
+            session()->regenerate(); // to avoid session fixation attack
+            //        redirect with a success flash message
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+//        display custom message if auth fail
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified.'
+        ]);
+[]
+        ### Other way to do
+//        return back()
+//            ->withInput()
+//            ->withErrors(['email' => 'Your provided credentials could not be verified.']);
+    }
+
     public function destroy()
     {
         auth()->logout();
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('register', [RegistrationController::class, 'create'])->middleware('guest');
-Route::post('register', [RegistrationController::class, 'store'])->middleware('guest');
-Route::post('logout', [SessionController::class, 'destroy']);
+Route::middleware('guest')->group(function () {
+    Route::get('register', [RegistrationController::class, 'create']);
+    Route::post('register', [RegistrationController::class, 'store']);
+    Route::get('login', [SessionController::class, 'create']);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::post('logout', [SessionController::class, 'destroy']);
+});
+
+Route::post('login', [SessionController::class, 'store']);
