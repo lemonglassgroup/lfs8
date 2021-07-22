9ced0c8c23ffe9bc212bd449dcdfd4a29da7834d
lemoncube1 <lemonglasgroup@protonmail.com>
2021-07-22 16:38:50 +0300

    episode 52 complete

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,22 @@
+<article class="flex bg-gray-100 border border-gray-200 rounded-xl p-6 space-x-4">
+    <div class="flex-shrink-0 ">
+        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
+    </div>
+
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+
+            <p class="text-xs">
+                Posted
+                <time>8 months ago</time>
+            </p>
+        </header>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus amet commodi dolore
+            dolorem fuga magni nam numquam obcaecati reprehenderit repudiandae sapiente sunt,
+            suscipit tempora temporibus veritatis. Assumenda dignissimos neque vitae?
+        </p>
+    </div>
+</article>
